4961247daa1d77627112f1d4eac8ac06265a5d05
CristinaNavas <cris.30a@hotmail.com>
2022-06-08 20:17:18 -0500

    Sprint 4.9

--- src/routes/productosRoute.js
+++ src/routes/productosRoute.js
@@ -1,11 +1,31 @@
+// ************ Require's ************
 const express = require("express");
 const router = express.Router();
+const path=require("path");
 
+// ************ Middlewares - (don't touch) ************
+const multer=require("multer");
+
+// ************ Multer Configuration ************
+const storage=multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,path.join(__dirname,"../../public/img/main"));
+    },
+    filename: (req,file,cb)=>{
+        console.log(file.originalname);
+        cb(null,"file-"+Date.now()+path.extname(file.originalname));
+    },
+})
+const upload= multer({storage:storage})
+
+// ************ Controller Require ************
 const productosController=require("../controller/productosController.js");
 
 router.get("/carrito",productosController.carrito);
 
-router.get("/productDetail/",productosController.productDetail);
+router.get("/productDetail/:id",productosController.productDetail);
 
-router.get("/productDetail/:id",productosController.productDetail2);
+router.get("/productCreate",productosController.productCreate);
+router.post("/productCreate", upload.single("image"),productosController.productSave);
+
 module.exports=router;
